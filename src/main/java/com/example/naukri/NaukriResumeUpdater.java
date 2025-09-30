317412e08dd6a6b1551145282d4f22ce871f6e92
ismail <ismailsh416@gmail.com>
2025-10-01 01:42:16 +0530

    chrome update 2

--- src/main/java/com/example/naukri/NaukriResumeUpdater.java
+++ src/main/java/com/example/naukri/NaukriResumeUpdater.java
@@ -5,30 +5,29 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.time.Duration;
 
 public class NaukriResumeUpdater {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         String username = System.getenv("NAUKRI_USERNAME");
         String password = System.getenv("NAUKRI_PASSWORD");
         String resumePath = Paths.get("src/main/resources/Ismail_Shaik.pdf").toAbsolutePath().toString();
-
-        WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--headless=new"); // container has no GUI
-
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+//        WebDriver driver = new ChromeDriver(options);
 
         try {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
